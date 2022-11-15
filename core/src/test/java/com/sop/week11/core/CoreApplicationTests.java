4b011514ca740b3455772faf5f2264bdfa942a13
Jeremy3times <than2314@gmail.com>
2022-11-15 11:21:48 +0700

    feat: 🎸 week 11

---
+++ core/src/test/java/com/sop/week11/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.week11.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ product-service/src/main/java/com/sop/chapter9/productservice/core/event/ProductCreatedEvent.java
@@ -0,0 +1,16 @@
+package com.sop.chapter9.productservice.core.event;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+    public ProductCreatedEvent(){
+
+    }
+}
